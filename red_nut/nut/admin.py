ed8759ada63a5dbe420134e5c606d79feb7de6c8
Alou <aloud7gmail.com>
2011-12-08 14:58:41 +0000

    add models Seat, DataNut

---
+++ red_nut/nut/admin.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# maintainer: alou
+
+from django.contrib import admin
+from nut.models import *
+
+admin.site.register(Seat)
+admin.site.register(DataNut)
--- red_nut/nut/models.py
+++ red_nut/nut/models.py
@@ -1,3 +1,25 @@
+#!/usr/bin/env python
+# encoding=utf-8
+# maintainer: Alou/Fadiga
+
+
+import datetime
 from django.db import models
 
-# Create your models here.
+
+class Seat(models.Model):
+    name = models.CharField(max_length=30, verbose_name=("Name"))
+    code = models.CharField(max_length=30, verbose_name=("Code"))
+
+    def __unicode__(self):
+        return u'%(name)s' % {"name": self.name}
+
+
+class DataNut(models.Model):
+    date = models.CharField(max_length=30, verbose_name=("Name"))
+    weight = models.CharField(max_length=30, verbose_name=("weight"))
+    heught = models.PositiveIntegerField(verbose_name=("heught"))
+    pb = models.PositiveIntegerField(verbose_name=("PB"))
+    danger_sign = models.CharField(max_length=30, verbose_name=("Danger sign"))
+    def __unicode__(self):
+        return u'%(name)s' % {"name": self.date}
