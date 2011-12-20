c6eec35511f0803b478cab1e1092ac49f22fcfc6
Alou <aloud7gmail.com>
2011-12-20 13:31:48 +0000

    vue details enfant

--- red_nut/nut/views/details_child.py
+++ red_nut/nut/views/details_child.py
@@ -1,17 +1,30 @@
 #!/usr/bin/env python
 # encoding=utf-8
-# maintainer: Fadiga
+# maintainer: Alou
 
 from django import forms
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, RequestContext, redirect
 from django.utils.translation import ugettext as _, ugettext_lazy
 from django.conf import settings
 
+from nut.models import Patient, DataNut
 
-def details_child(request):
+def details_child(request, *args, **kwargs):
+    num = kwargs["id"]
     category = 'details_child'
     context = {}
-    context.update({"category":category})
+    try:
+        patient = Patient.objects.filter(id = num)[0]
+        datanut = DataNut.objects.filter(patient__id=num)[0]
+        datanuts = DataNut.objects.filter(patient__id=num)
+        context.update({'category': category,
+                    'patient': patient,
+                    'datanut': datanut,
+                    'datanuts': datanuts})
+    except:
+        context.update({'error': 'aucun details'})
+
 
     return render(request, 'details_child.html', context)
