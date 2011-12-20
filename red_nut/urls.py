6f3cc0027d5b993c2e501486cae4133864a8ce3b
Fadiga <ibfadiga@gmail.com>
2011-12-20 10:20:07 +0000

    construction de url

--- red_nut/urls.py
+++ red_nut/urls.py
@@ -15,7 +15,7 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^/?$', views.dashboard.dashboard, name='index'),
     url(r'^children?$', views.children.children, name='children'),
-    url(r'^details_child?$', views.details_child.details_child, \
+    url(r'^details_child/(?P<id>\d+)$', views.details_child.details_child, \
                                                         name='details_child'),
     url(r'^health_center?$', views.health_center.health_center, \
                                                         name='health_center'),
