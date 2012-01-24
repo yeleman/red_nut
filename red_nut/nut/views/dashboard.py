2def8ad17a6de98d23c7673d1fbc158ad25ee931
Fadiga <ibfadiga@gmail.com>
2012-01-24 13:20:27 +0000

    Factorisation

--- red_nut/nut/views/dashboard.py
+++ red_nut/nut/views/dashboard.py
@@ -19,8 +19,6 @@ def dashboard(request):
 
     # Les ptients qui ne sont plus dans le programme
     out_program = ProgramIO.objects.filter(event=ProgramIO.OUT)
-    # Les données nutritionnelles
-    nutritional_data = DataNut.objects.all()
 
     def calculation_of_rates(nb, tnb):
         try:
@@ -64,12 +62,15 @@ def dashboard(request):
     except:
         avg_days = 0
     context.update({"avg_days": avg_days})
+
+    # Les données nutritionnelles
+    nutritional_data = DataNut.objects.all()
     # Gain de poids moyen
     patients = Patient.objects.all()
     list_weight = []
     for patient in patients:
         # les patients qui ont une donnée nutritionnelle
-        datanut_patient = nutritional_data.filter(patient__id=patient.id) \
+        datanut_patient = nutritional_data.filter(patient=patient) \
                                   .order_by('date')
         if datanut_patient:
             weight = diff_weight(datanut_patient[0].weight, \
@@ -82,25 +83,25 @@ def dashboard(request):
     context.update({"avg_weight": "%.2f" % avg_weight})
     # graphic
     try:
-        l_date = date_graphic(ProgramIO.objects.order_by("date")[0].date)
+        list_date = date_graphic(ProgramIO.objects.order_by("date")[0].date)
     except:
-        l_date = []
-    total_ = []
+        list_date = []
+    total_patient = []
     graph_date = []
     diagnose_mam = []
     diagnose_sam = []
     diagnose_ni = []
-    if l_date:
-        for da in l_date:
+    if list_date:
+        for dat in list_date:
             input_in_prog = ProgramIO.objects.filter(event=ProgramIO.SUPPORT,
-                                                                date__lte=da)
+                                                                date__lte=dat)
             out_in_prog = ProgramIO.objects.filter(event=ProgramIO.OUT,
-                                                             date__lte=da)
-            input_out_in_prog = [p for p in  input_in_prog if p.patient.id \
-                                 not in [i.patient.id for i in out_in_prog]]
-            data = DataNut.objects.order_by('date').filter(date__lte=da)
-            total_.append(input_in_prog.__len__() - out_in_prog.__len__())
-            graph_date.append(da.strftime('%d/%m'))
+                                                             date__lte=dat)
+            input_out_in_prog = [p for p in  input_in_prog if p.patient \
+                                 not in [i.patient for i in out_in_prog]]
+            data = DataNut.objects.order_by('date').filter(date__lte=dat)
+            total_patient.append(input_out_in_prog.__len__())
+            graph_date.append(dat.strftime('%d/%m'))
             l_diagnose = [diagnose_patient(d.muac, d.oedema) for d in data \
                                         if d.patient.id in [(i.patient.id) \
                                                 for i in input_out_in_prog]]
@@ -109,11 +110,6 @@ def dashboard(request):
                 diagnose_sam.append(l_diagnose.count('SAM'))
                 diagnose_ni.append(l_diagnose.count('SAM+'))
 
-    graph_data = [{'name': "Total", 'data': total_}, \
-                  {'name': "MAM", 'data': diagnose_mam}, \
-                  {'name': "MAS", 'data': diagnose_sam}, \
-                  {'name': "MAS+", 'data': diagnose_ni}]
-    context.update({"graph_date": graph_date, "graph_data": graph_data})
     # Diagnose
     try:
         MAM_count = diagnose_mam[-1]
@@ -132,7 +128,7 @@ def dashboard(request):
 
     # Nbre d'enfant dans le programme
     try:
-        children_in_program = total_[-1]
+        children_in_program = total_patient[-1]
     except:
         children_in_program = 0
 
@@ -144,10 +140,17 @@ def dashboard(request):
     # message
     received = Inbox.objects.count()
     sent = SentItems.objects.count()
+    # Graphic
+    graph_data = [{'name': "Total", 'data': total_patient}, \
+                  {'name': "MAM", 'data': diagnose_mam}, \
+                  {'name': "MAS", 'data': diagnose_sam}, \
+                  {'name': "MAS+", 'data': diagnose_ni}]
 
     context.update({"children_in_program": children_in_program, \
                     "sent": sent, "received": received, \
                     "MAM_count": MAM_count, "SAM_count": SAM_count, \
-                    "patients_late": patients_late, "NI_count": NI_count})
+                    "patients_late": patients_late, "NI_count": NI_count,
+                    "graph_date": graph_date, "graph_data": graph_data})
+
 
     return render(request, 'dashboard.html', context)
