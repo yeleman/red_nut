43ce8867b4b8e3a20ee41781e8835a0e418ec91c
Alou <aloud7gmail.com>
2012-01-24 09:48:47 +0000

    ajout du code de CSCOM

--- red_nut/nut/export_excel.py
+++ red_nut/nut/export_excel.py
@@ -45,23 +45,24 @@ def report_as_excel(health_centers):
 
     #entete liste des enfants
     sheet_patient.write(i_, 0, u"ID")
-    sheet_patient.write(i_, 1, u"CSCOM")
-    sheet_patient.write(i_, 2, u"Nom")
-    sheet_patient.write(i_, 3, u"Prénom")
-    sheet_patient.write(i_, 4, u"Mère")
-    sheet_patient.write(i_, 5, u"DDN")
-    sheet_patient.write(i_, 6, u"Sexe")
-    sheet_patient.write(i_, 7, u"Date d'enregistrement")
-    sheet_patient.write(i_, 8, u"Derniere visite")
-    sheet_patient.write(i_, 9, u"Poids")
-    sheet_patient.write(i_, 10, u"Taille")
-    sheet_patient.write(i_, 11, u"Perimètre brachial")
-    sheet_patient.write(i_, 12, u"Oedème")
-    sheet_patient.write(i_, 13, u"Date de visite")
-    sheet_patient.write(i_, 14, u"Dernier status")
-    sheet_patient.write(i_, 15, u"Evenement")
-    sheet_patient.write(i_, 16, u"Raison")
-    sheet_patient.write(i_, 17, u"Date de l'evenement")
+    sheet_patient.write(i_, 1, u"Code CSCOM")
+    sheet_patient.write(i_, 2, u"CSCOM")
+    sheet_patient.write(i_, 3, u"Nom")
+    sheet_patient.write(i_, 4, u"Prénom")
+    sheet_patient.write(i_, 5, u"Mère")
+    sheet_patient.write(i_, 6, u"DDN")
+    sheet_patient.write(i_, 7, u"Sexe")
+    sheet_patient.write(i_, 8, u"Date d'enregistrement")
+    sheet_patient.write(i_, 9, u"Derniere visite")
+    sheet_patient.write(i_, 10, u"Poids")
+    sheet_patient.write(i_, 11, u"Taille")
+    sheet_patient.write(i_, 12, u"Perimètre brachial")
+    sheet_patient.write(i_, 13, u"Oedème")
+    sheet_patient.write(i_, 14, u"Date de visite")
+    sheet_patient.write(i_, 15, u"Dernier status")
+    sheet_patient.write(i_, 16, u"Evenement")
+    sheet_patient.write(i_, 17, u"Raison")
+    sheet_patient.write(i_, 18, u"Date de l'evenement")
 
     for health_center in health_centers:
         consumptionreports = ConsumptionReport.objects\
@@ -78,7 +79,7 @@ def report_as_excel(health_centers):
                 sheet.write(i, 6, stock.used)
                 sheet.write(i, 7, stock.lost)
                 sheet.write(i, 8, stock.remaining())
-                sheet.write(i, 9, stock.period.full_name())
+                sheet.write(i, 9, stock.period.middle().strftime("%m-%Y"))
 
         patients = Patient.objects.filter(health_center=health_center)
         datanuts = DataNut.objects.filter(patient__health_center=health_center)
@@ -88,39 +89,45 @@ def report_as_excel(health_centers):
                 datanut_patients = datanuts.filter(patient__id=patient.id) \
                                           .order_by('date')
                 sheet_patient.write(i_, 0, patient.id)
-                sheet_patient.write(i_, 1, patient.health_center.name)
-                sheet_patient.write(i_, 2, patient.last_name)
-                sheet_patient.write(i_, 3, patient.first_name)
-                sheet_patient.write(i_, 4, patient.surname_mother)
-                sheet_patient.write(i_, 5, patient.birth_date\
-                                                  .strftime("%d/%m/%Y"))
-                sheet_patient.write(i_, 6, patient.sex)
-                sheet_patient.write(i_, 7, patient.create_date\
-                                                  .strftime("%d/%m/%Y"))
-                sheet_patient.write(i_, 8, patient.last_visit()\
-                                                  .strftime("%d/%m/%Y"))
-                sheet_patient.write(i_, 9, patient.last_data_nut().weight)
-                sheet_patient.write(i_, 10, patient.last_data_nut().height)
-                sheet_patient.write(i_, 11, patient.last_data_nut().muac)
-                sheet_patient.write(i_, 12, patient.last_data_nut()\
-                                                  .oedema)
-                sheet_patient.write(i_, 14, patient.last_data_event().event)
-                sheet_patient.write(i_, 16, patient.last_data_event().reason)
-                sheet_patient.write(i_, 17, patient.last_data_event().date
-                                                        .strftime("%d/%m/%Y"))
+                sheet_patient.write(i_, 1, patient.health_center.code)
+                sheet_patient.write(i_, 2, patient.health_center.name)
+                sheet_patient.write(i_, 3, patient.last_name)
+                sheet_patient.write(i_, 4, patient.first_name)
+                sheet_patient.write(i_, 5, patient.surname_mother)
+                sheet_patient.write(i_, 6, patient.birth_date\
+                                                  .strftime("%d-%m-%Y"))
+                sheet_patient.write(i_, 7, patient.sex)
+                sheet_patient.write(i_, 8, patient.create_date\
+                                                  .strftime("%d-%m-%Y"))
+                sheet_patient.write(i_, 9, patient.last_visit()\
+                                                  .strftime("%d-%m-%Y"))
+                sheet_patient.write(i_, 10, patient.last_data_nut().weight)
+                sheet_patient.write(i_, 11, patient.last_data_nut().height)
+                sheet_patient.write(i_, 12, patient.last_data_nut().muac)
+                sheet_patient.write(i_, 13, patient.last_data_nut()\
+                                                  .get_oedema_display().upper())
+                if patient.last_data_event().event == 'e':
+                    sheet_patient.write(i_, 15, 'ENTRE')
+                else:
+                    sheet_patient.write(i_, 15, 'SORTI')
+                sheet_patient.write(i_, 17, patient.last_data_event()\
+                                                .get_reason_display().upper())
+                sheet_patient.write(i_, 18, patient.last_data_event().date
+                                                        .strftime("%d-%m-%Y"))
                 if datanut_patients:
                     for data in datanut_patients:
                         if data != patient.last_data_nut():
                             i_ += 1
                             sheet_patient.write(i_, 0,\
                                         data.patient.id)
-                            sheet_patient.write(i_, 9, data.weight)
-                            sheet_patient.write(i_, 10, data.height)
-                            sheet_patient.write(i_, 11, data.muac)
-                            sheet_patient.write(i_, 12, data.oedema)
-                            sheet_patient.write(i_, 13, patient\
+                            sheet_patient.write(i_, 10, data.weight)
+                            sheet_patient.write(i_, 11, data.height)
+                            sheet_patient.write(i_, 12, data.muac)
+                            sheet_patient.write(i_, 13, data\
+                                                .get_oedema_display().upper())
+                            sheet_patient.write(i_, 14, patient\
                                     .last_data_nut()\
-                                    .date.strftime("%d/%m/%Y"))
+                                    .date.strftime("%d-%m-%Y"))
             try:
                 patient_programios = patient.programios.order_by('date')
                 for pp in patient_programios:
@@ -128,10 +135,14 @@ def report_as_excel(health_centers):
                         i_ += 1
                         sheet_patient.write(i_, 0,\
                                         pp.patient_id)
-                        sheet_patient.write(i_, 15, pp.event)
-                        sheet_patient.write(i_, 16, pp.reason)
-                        sheet_patient.write(i_, 17, pp.date
-                                    .strftime("%d/%m/%Y"))
+                        if patient.last_data_event().event == 'e':
+                            sheet_patient.write(i_, 16, 'ENTRE')
+                        else:
+                            sheet_patient.write(i_, 16, 'SORTI')
+                        sheet_patient.write(i_, 17, pp\
+                                                .get_reason_display().upper())
+                        sheet_patient.write(i_, 18, pp.date
+                                    .strftime("%d-%m-%Y"))
             except:
                 pass
 
