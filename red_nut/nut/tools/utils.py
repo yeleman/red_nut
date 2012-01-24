a1da8852421912bf9510c1681930bb38a72ace5a
Fadiga <ibfadiga@gmail.com>
2012-01-24 09:12:05 +0000

    Correction date_graphic

--- red_nut/nut/tools/utils.py
+++ red_nut/nut/tools/utils.py
@@ -65,10 +65,10 @@ def diff_weight(p1, p2):
 
 
 def date_graphic(date_on):
-    l_date = [date_on]
+    l_date = []
     while(date_on <= date.today()):
-        date_on += timedelta(7)
         l_date.append(date_on)
+        date_on += timedelta(7)
     return l_date
 
 
