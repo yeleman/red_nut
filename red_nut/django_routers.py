9d245e7740e823cba7660ec7129c9ada311f9e16
renaud gaudin <rgaudin@gmail.com>
2012-10-05 15:25:55 +0000

    south migrations are part of rednut DB (data)

--- red_nut/django_routers.py
+++ red_nut/django_routers.py
@@ -2,25 +2,25 @@
 # -*- coding: utf-8 -*-
 # vim: ai ts=4 sts=4 et sw=4 nu}
 
-APP='nut'
-DATABASE='rednut'
+APPS = ('nut', 'south')
+DATABASE = 'rednut'
 
 
 class RedNutRouter(object):
 
     def allow_syncdb(self, db, model):
-        if model._meta.app_label == APP:
+        if model._meta.app_label in APPS:
             return db == DATABASE
         else:
             return False if db == DATABASE else None
         return None
 
     def db_for_read(self, model, **hints):
-        if model._meta.app_label == APP:
+        if model._meta.app_label in APPS:
             return DATABASE
         return None
 
     def db_for_write(self, model, **hints):
-        if model._meta.app_label == APP:
+        if model._meta.app_label in APPS:
             return DATABASE
         return None
